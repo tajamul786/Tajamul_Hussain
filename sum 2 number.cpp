791e801dbbb31ebe10a3d1dc5d94305ac4c317ac
tajamul786 <tajamulhussain90@gmail.com>
2016-09-26 12:57:42 +0500

    Add files via upload

---
+++ sum 2 number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a ,b,sum;
+cout<<"Enter 1st Number:"<<endl;
+cin>>a;
+cout<<"Enter 2nd Number:"<<endl;
+cin>>b;
+sum=a+b;
+cout<<"sum is"<<endl<<sum;
+return 0;
+}
